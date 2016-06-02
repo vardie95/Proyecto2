83882410344f7b2b2a17ce04c523ec73e3da87b3
vardie95 <vardie95@gmail.com>
2016-06-01 22:52:30 -0600

    Creacion Varias Ventanas

--- ProyectoCerveza/src/Interfaz/Inicio.java
+++ ProyectoCerveza/src/Interfaz/Inicio.java
@@ -41,6 +41,7 @@ public class Inicio extends javax.swing.JFrame {
         jMenu3 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
         jMenu5 = new javax.swing.JMenu();
 
@@ -69,6 +70,7 @@ public class Inicio extends javax.swing.JFrame {
         jMenu3.setPreferredSize(new java.awt.Dimension(190, 19));
 
         jMenuItem1.setText("Persona");
+        jMenuItem1.setPreferredSize(new java.awt.Dimension(150, 20));
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem1ActionPerformed(evt);
@@ -77,12 +79,22 @@ public class Inicio extends javax.swing.JFrame {
         jMenu3.add(jMenuItem1);
 
         jMenuItem3.setText("Cerveza");
+        jMenuItem3.setPreferredSize(new java.awt.Dimension(150, 20));
         jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem3ActionPerformed(evt);
             }
         });
         jMenu3.add(jMenuItem3);
+
+        jMenuItem4.setText("Fermentación");
+        jMenuItem4.setPreferredSize(new java.awt.Dimension(150, 20));
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem4);
 
         jMenuBar1.add(jMenu3);
 
@@ -138,6 +150,16 @@ public class Inicio extends javax.swing.JFrame {
         jPanel1.revalidate();
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+         RegistroFermentacion panelIni= new RegistroFermentacion();
+        jPanel1.removeAll();
+        jPanel1.add(panelIni);
+        jPanel1.repaint();
+        jPanel1.revalidate();
+    
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -182,6 +204,7 @@ public class Inicio extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
